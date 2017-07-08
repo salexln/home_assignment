ba52aa5a7e7dfd9f5fc00e2540e78fb892ee6461
Alex <salexln@gmail.com>
2017-07-08 18:36:25 +0300

    reading is also in thread

--- src/frequent_synonym_groups.py
+++ src/frequent_synonym_groups.py
@@ -1,13 +1,13 @@
 
 import argh
 from connect import StdOutListener
-# from synonym_group import SynonymGroup
+from synonym_group import SynonymGroup
 from tweepy import OAuthHandler
 from tweepy import Stream
 import Queue
 import threading
 import time
-
+from nltk.corpus import wordnet as wn
 
 access_token = '883627537032785921-S3YvAf4wWX8ZZ2uMsRSecD3CJDYUzbk'
 access_token_secret = 'mLNgapLcew5fiZ1FUf6D7wlAivJDpOjZ1eBjv8Di9G6zm'
@@ -33,15 +33,37 @@ def get_tweeter_stream_in_thread(tweet_queue):
     t = threading.Thread(target=get_tweeter_stream, kwargs={'tweet_queue':tweet_queue})
     t.start()
 
+def add_to_synonym_group(tweet_queue):
+    # import pdb; pdb.set_trace()
+    print 'bbb'
+    while tweet_queue.qsize() != 0:
+        print 'aaa'
+        tweet = tweet_queue.get()
+        print tweet
+    #
+    # for word in tweet:
+    #     synsets = wordnet.synsets(test)
+    #     s = set()
+    #     for synset in synsets:
+    #         s.add(synset.name().split('.')[0].encode('utf-8')
+        # print s
+
 @argh.arg('--synonyms', help='Number of synonyms', type=str, required=True)
 def main(**kwargs):
     # print int(kwargs['synonyms'])
     tweet_queue = Queue.Queue()
-    print 'Starting tweeter stream'
+    # print 'Starting tweeter stream'
     get_tweeter_stream_in_thread(tweet_queue)
-    time.sleep(20)
-    print 'Tweeter stream is done, read {} tweets'.format(tweet_queue.qsize())
-    words_to_groups = {}
+    time.sleep(10)
+    # print 'Tweeter stream is done, read {} tweets'.format(tweet_queue.qsize())
+
+    t = threading.Thread(target=add_to_synonym_group, kwargs={'tweet_queue':tweet_queue})
+    t.start()
+
+
+
+
+    # words_to_groups = {}
 
 
 if __name__ == '__main__':
