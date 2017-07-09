6662e90c58d9af1254334142246e15f66d42791b
Alex <salexln@gmail.com>
2017-07-09 21:38:06 +0300

    changes in FrequentSynonyms

--- src/connect.py
+++ src/connect.py
@@ -5,9 +5,6 @@ from tweepy import Stream
 import timeit
 import Queue
 
-
-
-
 """
 Dependencies
 
@@ -24,16 +21,13 @@ class StdOutListener(StreamListener):
         self._tweet_queue = tweet_queue
 
     def on_data(self, data):
-        # import pdb; pdb.set_trace()
         curr_time = timeit.default_timer()
-        # if (curr_time - self._start_time > 60):
-        #     return False
-
         json_data = json.loads(data)
+
         if self._is_english(json_data):
             splitted = json_data['text'].split()
             text = [x.encode('utf-8') for x in splitted]
-            # print text
+
             self._number_of_tweets += 1
             self._tweet_queue.put((curr_time, text))
 
@@ -43,24 +37,11 @@ class StdOutListener(StreamListener):
         print status
 
     def _is_english(self, json):
+        """
+        Checks if the tweet is in english
+        """
         lang = json.get('lang')
         if lang == u'en':
             return True
 
         return False
-
-
-
-if __name__ == '__main__':
-
-    #This handles Twitter authetification and the connection to Twitter Streaming API
-    listener = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth, listener)
-
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    # stream.filter(track=['python', 'javascript', 'ruby'])
-    stream.sample()
-
-    print 'Total num of tweets {}'.format(listener._number_of_tweets)
--- src/frequent_synonym_groups.py
+++ src/frequent_synonym_groups.py
@@ -1,4 +1,3 @@
-import argh
 from connect import StdOutListener
 from synonym_group import SynonymGroup
 from tweepy import OAuthHandler
@@ -7,6 +6,7 @@ import Queue
 import threading
 import time
 from nltk.corpus import wordnet as wn
+import argparse
 
 access_token = '883627537032785921-S3YvAf4wWX8ZZ2uMsRSecD3CJDYUzbk'
 access_token_secret = 'mLNgapLcew5fiZ1FUf6D7wlAivJDpOjZ1eBjv8Di9G6zm'
@@ -31,11 +31,17 @@ class FrequentSynonyms(object):
         print 'queue size: {}'.format(self._tweet_queue.qsize())
 
     def start_tweeter_stream_in_thread(self):
+        """
+        Starts new thread that will run the twitter stream
+        """
         print '*** Staring Twitter stream ***'
         t = threading.Thread(target=self._start_tweeter_stream)
         t.start()
 
     def get_synonym_group_from_word(self, word):
+        """
+        Using WordNet returns the synonym group for a given word
+        """
         synonym_group = []
 
         # we do not want number and words like 'a' and 'I'
@@ -54,6 +60,9 @@ class FrequentSynonyms(object):
         return synonym_group
 
     def _add_to_synonym_group(self):
+        """
+        Adds the synonym group to the data structure
+        """
         while True:
             # this may be faster than the read from Twitter stream, so we need to wait
             if self._tweet_queue.qsize() == 0:
@@ -83,18 +92,23 @@ class FrequentSynonyms(object):
                     synonym_group.update_appearance(word)
 
     def start_adding_to_synonym_group_in_thread(self):
+        """
+        Runs the thread that adds synonym groups to data structure
+        """
         t = threading.Thread(target=self._add_to_synonym_group)
         t.start()
 
     def _find_most_frequent_synonym_groups(self):
+        """
+        Retruns N nost frequent synonym groups every 2 seconds
+        """
         while True:
             time.sleep(2)
 
             sorted_groups = sorted(self._synonym_groups, reverse=True)
 
             if len(sorted_groups) > 0:
-
-                print '*** Most frequent appearances: ***'
+                print '\n***** Most frequent appearances: *****'
                 idx = 0
                 while idx < self._number_of_top_synonyms:
                     sorted_groups[idx].print_group()
@@ -105,23 +119,22 @@ class FrequentSynonyms(object):
                         break
 
     def find_most_frequent_synonym_groups_in_thread(self):
+        """
+        Runs the thread that returns N synonym groups
+        """
         t = threading.Thread(target=self._find_most_frequent_synonym_groups)
         t.start()
 
 
-@argh.arg('--synonyms', help='Number of synonyms', type=str, required=True)
-def main(**kwargs):
-
-    frequent_synonyms = FrequentSynonyms(int(kwargs['synonyms']))
-
+def run(args):
+    frequent_synonyms = FrequentSynonyms(int(args.synonyms))
     frequent_synonyms.start_tweeter_stream_in_thread()
-
     frequent_synonyms.start_adding_to_synonym_group_in_thread()
-
     frequent_synonyms.find_most_frequent_synonym_groups_in_thread()
 
 
 if __name__ == '__main__':
-    parser = argh.ArghParser()
-    parser.set_default_command(main)
-    parser.dispatch()
+    parser = argparse.ArgumentParser(description='parser')
+    parser.add_argument('--synonyms', help='Number of synonyms', type=str, required=True)
+    args = parser.parse_args()
+    run(args)
